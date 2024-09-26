35f2a11c76529aff2e1e8a46b725574b93cc5e39
Jonath61 <jonath.even@gmail.com>
2024-09-26 18:01:55 +0200

    json mis en place

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,8 +4,8 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
     new Route("/galerie", "La galerie", "/pages/galerie.html", [], "/js/galerie.js"),
-    new Route("/signin", "Connection", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
+    new Route("/signin", "Connection", "/pages/auth/signin.html", [], "/js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", [], "/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html", ["connected"]),
     new Route("/editpassword", "Modifier le mot de passe", ["viewver, admin, streamer"], "/pages/auth/editpassword.html"),
     new Route("/streamer", "Streamer", "/pages/auth/streamer.html", ["admin"], "/js/auth/streamer.js"),
--- js/auth/signin.js
+++ js/auth/signin.js
@@ -1,11 +1,26 @@
 const mailInput = document.getElementById("EmailInput");
 const passwordInput = document.getElementById("PasswordInput");
 const btnSignin = document.getElementById("btnSignin");
+import fs from 'fs';
+const data = JSON.parse(fs.readFileSync('../../json/data.json', 'utf-8'));
 
 btnSignin.addEventListener("click", checkCredentials);
 
 function checkCredentials(){
+    var mail = input.value;
+    var password = input.value;
+
+    var CanConnect = false;
+
+    data.utilisateur.forEach(element => {
+        if(element.email == mail && element.password == password){
+            canConnect = true;
+        }
+    })
+
     //Ici, il faudra appeler l'API pour vérifier les credentials en BDD
+
+    //for(var i = 0; i < Data["utilisateur"],lenght; i++)
     
     if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){
         //Il faudra récupérer le vrai token
